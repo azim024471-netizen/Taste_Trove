af99ed7d4dd0f0d6a0adf3023528c9a85f27e90b
azim024471-netizen <azim024471@gmail.com>
2026-07-01 12:36:27 +0600

    trying to fix the bug of faviourites recpire

--- src/lib/api_actions/favourites.js
+++ src/lib/api_actions/favourites.js
@@ -2,6 +2,16 @@ import {  serverFetch } from "../core_function/server"
 
 
 
-export const myFavouritesRecpies = async (userid)=>{
-    return serverFetch(`/api/myFavorites/${userid}`)
+// export const myFavouritesRecpies = async (userid)=>{
+//     return serverFetch(`/api/myFavorites/${userid}`)
+// }
+
+ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
+
+export const myFavouritesRecpies = async (userid) => {
+    const res = await fetch(`${BASE_URL}/api/myFavorites/${userid}`, { 
+        cache: 'no-store' 
+    });
+    return res.json();
 }
